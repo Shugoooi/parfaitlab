6dd31e913bde963516e4817507c11d382f3f6a91
shugoooi <ghbjy6@gmail.com>
2018-11-02 02:04:32 +0900

    __ADD__ a bit change

--- haskell/SewingMaster/src/SomeSewing.hs
+++ haskell/SewingMaster/src/SomeSewing.hs
@@ -1,17 +1,19 @@
---Constants
-let trival_head = Status { fields      = [ Field (0, 1) 210
-                                         , Field (1, 0) 210
-                                         , Field (1, 1) 210
-                                         , Field (1, 2) 210 ]
-                         , concentrate = 213
-                         , clothType   = REPAIR
-                         , powerCycle  = [1, 0, 1.5, 0.5, 2] }
+import Data.List
 
+--Constants
+trivalHead = Status { fields      = [ Field (0, 1) 210
+                                     , Field (1, 0) 210
+                                     , Field (1, 1) 210
+                                     , Field (1, 2) 210 ]
+                     , concentrate = 213
+                     , clothType   = REPAIR
+                     , powerCycle  = [1, 0, 1.5, 0.5, 2] }
 
 type Row = Int
 type Col = Int
+type Coordinate = (Row, Col)
 type Damage = Int
-data Field = Field (Row, Col) Damage
+data Field = Field Coordinate Damage deriving (Show)
 coordinate :: Field -> (Row,Col)
 coordinate (Field (x,y) _) = (x,y)
 
@@ -19,10 +21,10 @@ type SewPower = Float
 type PowerCycle = [SewPower]
 
 data ClothType = RAINBOW | REPAIR
-
+type Concentrate = Int
 
 data Status = Status { fields      :: [Field]
-                     , concentrate :: Int
+                     , concentrate :: Concentrate
                      , clothType   :: ClothType
                      , powerCycle  :: PowerCycle }
 
@@ -39,9 +41,22 @@ data Skill = Skill { skillName    :: String
 -- act :: Status -> Skill -> Status
 act :: Field -> String -> Field
 act (Field (x, y) 110) "sewing" = Field (x, y) (110-12)
+takeDamage :: [Field] -> Field -> [Field]
+takeDamage fs f = first ++ [takeDamage' (head second) f] ++ tail second
+    where (first, second) = break (\x -> coordinate x == coordinate f) fs
+
+takeDamage' :: Field -> Field -> Field
+takeDamage' (Field a dx) (Field b dy) = Field a (dx-dy)
+
+updateField :: Status -> [Field] -> Status
+updateField status field = status { fields=field }
+updateConcentrate :: Status -> Concentrate -> Status
+updateConcentrate status concentrate = status { concentrate=concentrate }
 -- act' :: Status -> String -> Field
 -- act' status "sewing" = Field (x, y) (110-12)
 -- detect (Status a b c d) (x,y) = find (\x -> 
+detect :: [Field] -> Coordinate -> Maybe Field
+detect f c = find (\x -> coordinate x == c) f
 
 -- Å“K‰ğQÆ
-main = do print "oho"
+main = do print $ detect (fields trivalHead) (1,1)
